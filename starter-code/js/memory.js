b4f7d1e60f97e28543861d7776337f5dbdeb6071
Peacock-Co <pavaoleonardo@hotmail.com>
2019-11-30 14:47:07 +0100

    PP till this moment

--- starter-code/js/memory.js
+++ starter-code/js/memory.js
@@ -1,9 +1,16 @@
 class MemoryGame {
-  constructor(cards){
+  constructor(cards) {
     this.cards = cards;
+    this.pickedCards = [];
+    this.pairsClicked = 0;
+    this.pairsGuessed = 0;
     // add the rest of the class properties here
   }
-  shuffleCards() {}
+  shuffleCards() {
+    for (let i = 0; i < this.cards.length; i++) {
+      this.cards[Math.floor(Math.random() * this.cards.length)];
+    }
+  }
   checkIfPair(card1, card2) {}
   isFinished() {}
 }
